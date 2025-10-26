6db710dfe0982b0e76cc144d8406eba186239e52
1lelanta <kekikoki4@gmail.com>
2025-10-26 19:56:57 +0300

    add sign in api controller

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -52,5 +52,37 @@ export const signUp = async(req,res,next)=>{
 
 // sign in logic
 export const signIn = async(req,res,next)=>{
+    try {
+        const {email,password}=req.body;
+        const user = await User.findOne({email});
+
+        if(!user){
+            const error = new Error("user not found");
+            error.statusCode=404;
+            throw error;
+        }
+
+        const isPasswordValid = await bcrypt.compare(password,user.password);
+
+        if(!isPasswordValid){
+            const error = new Error("Invalid password");
+            error.statusCode=401;
+            throw error;
+        }
+
+        const token = jwt.sign({userId:user._id},JWT_SECRET,{expiresIn:JWT_EXPIRES_IN});
+
+        res.status(200).json({
+            success:true,
+            message:"User signed in successfully",
+            data:{
+                token,
+                user
+            }
+        });
+    } catch (error) {
+        next(error);
+        
+    }
 
 }

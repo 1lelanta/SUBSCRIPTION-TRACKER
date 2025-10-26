cd45bc7a7ac497c7b6d0573378f45e35bbe5bf02
1lelanta <kekikoki4@gmail.com>
2025-10-26 20:21:41 +0300

    add api logic to get specific user

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -11,3 +11,19 @@ export const getUsers = async(req,res,next)=>{
         
     }
 }
+
+export const getUser = async(req,res,next)=>{
+    try {
+        const user = await User.findById(req.params.id).select(-password);
+        if(!user){
+            const error = new Error('user not found');
+            error.statusCode = 404;
+            throw error
+        }
+
+        res.status(200).json({success:true,data:user});
+    } catch (error) {
+        next(error);
+        
+    }
+}

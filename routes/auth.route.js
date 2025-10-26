ea1ac7026a7cfe4a6da1fe6655b6d984f04754de
1lelanta <kekikoki4@gmail.com>
2025-10-26 20:12:36 +0300

    add user controller to fetch all the user in database

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,10 +1,10 @@
 import { Router } from "express";
-import { signUp } from "../controllers/auth.controller.js";
+import { signIn, signUp } from "../controllers/auth.controller.js";
 
 const authRouter = Router();
 
 authRouter.post('/sign-up',signUp);
-authRouter.post('/sign-in',(req,res)=>res.send({title:'Sign in'}));
+authRouter.post('/sign-in',signIn);
 authRouter.post('/sign-out',(req,res)=>res.send({title:'Sign out'}));
 
 export default authRouter;

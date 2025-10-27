7c21e734f776897b8027f9ee0c8f358bb4d175d3
1lelanta <kekikoki4@gmail.com>
2025-10-27 21:52:59 +0300

    add user authorization logic to the api

--- routes/user.route.js
+++ routes/user.route.js
@@ -1,12 +1,12 @@
 import { Router } from "express";
-import { signUp } from "../controllers/auth.controller.js";
 import { getUser, getUsers } from "../controllers/user.controller.js";
+import authorize from "../middlewares/auth.middleware.js";
 
 const userRouter=Router();
 
 userRouter.get('/',getUsers);
 
-userRouter.get('/:id',getUser);
+userRouter.get('/:id', authorize, getUser);
 
 userRouter.post('/',(req,res)=>res.send({title:'get user detail'}));
 

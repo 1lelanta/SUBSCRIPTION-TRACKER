df49f08733e4831915ce1010c0fb9c42a9174419
1lelanta <kekikoki4@gmail.com>
2025-10-31 19:27:33 +0300

    add get all user controller

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -86,3 +86,7 @@ export const signIn = async(req,res,next)=>{
     }
 
 }
+
+export const signOut = async(req,res,next)=>{
+
+}
--- controllers/subscription.controller.js
+++ controllers/subscription.controller.js
@@ -1,6 +1,6 @@
 import Subscription from "../models/subscription.model"
 
- const createSubscription=async(req,res,next)=>{
+ export const createSubscription=async(req,res,next)=>{
     try {
         const subscription = await Subscription.create({
             ...req.body,
@@ -16,4 +16,18 @@ import Subscription from "../models/subscription.model"
     }
 }
 
-export default createSubscription;
+export const getUserSubscriptions =  async(req,res,next)=>{
+    try {
+        if(req.user.id !==req.params.id){
+            const error = new Error('You are not the owner of this account');
+            error.status=401;
+            throw error;
+        }
+
+        const subscriptions = await Subscription.find({user:req.params.id});
+
+        res.status(200).json({success:true,data:subscriptions})
+    } catch (error) {
+        next(error);
+    }
+}

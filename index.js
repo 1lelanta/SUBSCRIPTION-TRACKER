24564b58522a3da5c05122b08051391fa0b52e49
1lelanta <kekikoki4@gmail.com>
2025-11-01 21:15:32 +0300

    add message to check if server is running

--- index.js
+++ index.js
@@ -11,25 +11,35 @@ import workflowRouter from './routes/workflow.route.js';
 
 dotenv.config();
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
+
+// Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(arcjetMiddleware);
-app.use('/api/v1/workflows',workflowRouter);
 
+// Root route for testing
+app.get('/', (req, res) => {
+  res.send('Subscription Tracker API is running!');
+});
+
+// API routes
+app.use('/api/v1/workflows', workflowRouter);
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/subscription', subscriptionRouter);
 
+// Error middleware (should be last)
 app.use(errorMiddlewar);
 
+// Start server
 const startServer = async () => {
   await connectToDatabase();
   app.listen(PORT, () => {
-    console.log(`app is running on http://localhost:${PORT}`);
+    console.log(`App is running on http://localhost:${PORT}`);
   });
 };
 
